9b52b612c83f6290e3207112081ddcceaf3043b7
Abhimanyu Tiwari <theabhimanyutiwari@gmail.com>
2026-09-03 21:49:07 +0530

    Reorganize DSA folder structure

--- DSA/PATTERN_PROGRAMMING/p7.js
+++ DSA/PATTERN_PROGRAMMING/p7.js
@@ -3,9 +3,12 @@ let n = Number(prompt("Enter The Number"))
 
 for(let i=1;i<=n;i++){
    for(j=1;j<=n;j++){
-    if(i==j || (i+j == n+1))process.stdout.write("* ");
+    if(i==j)process.stdout.write("* ");
      else process.stdout.write("  ");
    }
-   
+   for(j=1;j<=n;j++){
+    if((i+j == n+1))process.stdout.write("* ");
+     else process.stdout.write("  ");
+   }
    console.log();
 }
