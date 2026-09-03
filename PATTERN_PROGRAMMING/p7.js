c118ccc86a521b019dab1f2e8a7442662ad5beea
Abhimanyu Tiwari <theabhimanyutiwari@gmail.com>
2026-09-03 20:01:59 +0530

    Pattern-7

---
+++ PATTERN_PROGRAMMING/p7.js
@@ -0,0 +1,11 @@
+const prompt = require("prompt-sync")();
+let n = Number(prompt("Enter The Number"))
+
+for(let i=1;i<=n;i++){
+   for(j=1;j<=n;j++){
+    if(i==j || (i+j == n+1))process.stdout.write("* ");
+     else process.stdout.write("  ");
+   }
+   
+   console.log();
+}
