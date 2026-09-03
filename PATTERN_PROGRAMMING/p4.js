979ef546dbf4e14b31774b6db2310f881020abfb
Abhimanyu Tiwari <theabhimanyutiwari@gmail.com>
2026-09-03 18:56:43 +0530

    Pattern 4

---
+++ PATTERN_PROGRAMMING/p4.js
@@ -0,0 +1,9 @@
+const prompt = require("prompt-sync")();
+let n = Number(prompt("Enter The Number"))
+
+for(let i=1;i<=n;i++){
+    for(j=1;j<=i;j++){
+        process.stdout.write(String.fromCharCode(64 + j));
+    }
+    console.log();
+}
